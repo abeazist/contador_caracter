cea5dcb64b45d8dc4045421216f35addd13c1a49
beatriz <128046453+abeazist@users.noreply.github.com>
2024-04-24 11:47:35 -0300

    feito js

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 const inputTexto = document.querySelector("[input-texto]")
 const divContador = document.querySelector("[txt-contador]")
 const btnContar = document.querySelector("[btn-contar]")
-const contar = document.querySelector("[text-contador]")
 
 
 inputTexto.addEventListener("input",handleInputTexto)
 btnContar.addEventListener("click",handleBtnContar)
+btnContar.addEventListener("click",handleInputTexto)
 
-let modo = "caracteres"
+let estado = "caracteres"
+
 function handleInputTexto(evento){
-    console.log(evento.target.value)
+    let palavras
+    if (estado == "caracteres"){
+        divContador.innerText = inputTexto.value.length + " caracteres"
+
+    }   else {       
+        palavras = (inputTexto.value.trim() == "" ? 0 : inputTexto.value.trim().split(/\s+/).length)
+        divContador.innerText = palavras + " Palavras"
+    }
 }
 
 function handleBtnContar(evento){
-    if (modo === "caracteres"){
-        modo = "palavras"
+    if (estado === "caracteres"){
+        estado = "palavras"
         btnContar.innerText = "Contar Palavras"
-    } else {
-        modo = "caracteres"
+    }else {
+        estado = "caracteres"
         btnContar.innerText = "Contar Caracteres"
-    }
 }
-
-
-function contador(){
-    let somar = document.getElementsByClassName("contador").value(0)
-    let texto = inputTexto.value
-    for (let n of btnContar){
-        somar+=1
-        
-        document.querySelector("[txt-contador}").innerText = somar
-    }
 }
-
